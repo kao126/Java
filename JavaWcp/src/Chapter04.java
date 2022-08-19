6236fef3ca6d9781a0d555fea89cad7e69d6c13d
kao126 <76607249+kao126@users.noreply.github.com>
2022-08-19 16:14:55 +0900

    java 変数

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str = "Kanaru Oshima";
+		System.out.println(str);
+
+		str = "Kyle";
+		System.out.println(str);
+
+		int NUM = 7;
+		System.out.println(NUM);
+	}
+}

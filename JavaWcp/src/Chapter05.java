a697c810580311f4e58734ee78288050f273abd8
kao126 <76607249+kao126@users.noreply.github.com>
2022-08-20 23:26:28 +0900

    型種類

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,26 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 3L;
+		System.out.println(long1);
+		
+		double double1 = 3.14;
+		System.out.println(double1);
+		
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+		
+		Float float1 = 3.14F;
+		System.out.println(float1);
+		
+		Integer integer1 = 7;
+		System.out.println(integer1);
+		
+		Character character1 = 'あ';
+		System.out.println(character1);
+		
+		int[] array1 = new int[] {1, 2, 3, 4, 5};
+		System.out.println(array1[3]);
+	}
+
+}
